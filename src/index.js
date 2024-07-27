691d0a86cf1c9d1121b2ed0e2071c6f5f8a5282a
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-27 21:24:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 export { default as LazyImage } from './LazyImage';
 export { default as LazyIframe } from './LazyIframe';
 export { default as LazyBackgroundImage } from './LazyBackgroundImage';
