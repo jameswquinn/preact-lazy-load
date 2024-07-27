1f56028604bc38ed0f80b805528cf74023527747
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-27 21:45:41 +0100

    Create rollup.config.mjs

---
+++ rollup.config.mjs
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from '@rollup/plugin-terser';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    terser()
+  ]
+};
