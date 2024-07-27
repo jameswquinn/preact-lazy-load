67e9a721bf313ff11bb32b965264dd1001bf7097
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-27 22:18:55 +0100

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import { terser } from '@rollup/plugin-terser';
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'esm',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true
-    }
-  ],
+  input: "src/index.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "esm",
+  },
   plugins: [
-    resolve(),
-    commonjs(),
-    babel({ babelHelpers: 'bundled' }),
-    terser()
-  ]
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"],
+      plugins: [
+        [
+          "@babel/plugin-transform-react-jsx",
+          { pragma: "h", pragmaFrag: "Fragment" },
+        ],
+      ],
+      extensions: [".js", ".jsx"],
+    }),
+    terser(),
+  ],
 };
